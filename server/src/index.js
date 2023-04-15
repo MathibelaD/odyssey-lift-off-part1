7e3197eef7302068565bf563ece8ca3c3abf26a2
MathibelaD <dineomathibela161@gmail.com>
2023-04-15 23:25:10 +0200

    context init

--- server/src/index.js
+++ server/src/index.js
@@ -3,16 +3,24 @@ const { startStandaloneServer } = require('@apollo/server/standalone')
 const { addMocksToSchema } = require("@graphql-tools/mock");
 const { makeExecutableSchema } = require("@graphql-tools/schema");
 const typeDefs = require('./schema');
-const mocks = require('./mockData');
+const resolvers = require('./resolver');
+const TrackAPI = require('./datasources/track-api')
 // import { mocks } from './mockData';
 
 const startMyServer = async () => {
     const server = new ApolloServer({ 
-        schema: addMocksToSchema({
-        schema: makeExecutableSchema({ typeDefs }),
-        mocks,
-      }),})
-    const {url} = await startStandaloneServer(server)
+        typeDefs,
+        resolvers,
+      })
+    const {url} = await startStandaloneServer(server,{
+      context: async () => {
+        return {
+          dataSources : {
+            trackAPI: new TrackAPI()
+          }
+        }
+      }
+    })
     console.log(`server is running at ${url}`)
     // console.log(server)
 }
--- server/src/resolver.js
+++ server/src/resolver.js
@@ -2,8 +2,17 @@ const resolvers = {
     Query: {
         // returns an array of Tracks that will be used to populate
         // the homepage grid of our web client
-        tracksForHome: () => {},
-      }
+        tracksForHome: (_,__,{dataSources}) => {
+            console.log(dataSources)
+            return dataSources.trackAPI.getTracksForHome(); 
+        },
+      },
+      Track: {
+        author: ({authorId}, _, { dataSources }) => {
+            console.log(dataSources)
+            return dataSources.trackAPI.getAuthor(authorId);
+        }, 
+      },
 }
 
 module.exports = resolvers
