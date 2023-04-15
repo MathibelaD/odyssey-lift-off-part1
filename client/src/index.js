faac7c73a574f0c35b76799ab5b8e96dc05e13b3
MathibelaD <dineomathibela161@gmail.com>
2023-04-15 22:16:44 +0200

    apollo

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 import ReactDOM from 'react-dom';
 import GlobalStyles from './styles';
 import Pages from './pages';
 
+const client = new ApolloClient({
+  // options go here
+  uri: 'http://localhost:4000',
+  cache: new InMemoryCache(),
+});
+
 ReactDOM.render(
-  <React.StrictMode>
+  <ApolloProvider client={client}>
     <GlobalStyles />
     <Pages />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </ApolloProvider>,
+  document.getElementById("root")
 );
--- server/src/index.js
+++ server/src/index.js
@@ -1,1 +1,20 @@
-//TODO
+const {ApolloServer} = require('@apollo/server');
+const { startStandaloneServer } = require('@apollo/server/standalone')
+const { addMocksToSchema } = require("@graphql-tools/mock");
+const { makeExecutableSchema } = require("@graphql-tools/schema");
+const typeDefs = require('./schema');
+const mocks = require('./mockData');
+// import { mocks } from './mockData';
+
+const startMyServer = async () => {
+    const server = new ApolloServer({ 
+        schema: addMocksToSchema({
+        schema: makeExecutableSchema({ typeDefs }),
+        mocks,
+      }),})
+    const {url} = await startStandaloneServer(server)
+    console.log(`server is running at ${url}`)
+    // console.log(server)
+}
+
+startMyServer()
