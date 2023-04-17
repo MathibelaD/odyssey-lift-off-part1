7555eb3f855c1e3b014ac7928fe4a1d6d78bf830
MathibelaD <dineomathibela161@gmail.com>
2023-04-17 08:59:15 +0200

    mutations

--- server/src/datasources/track-api.js
+++ server/src/datasources/track-api.js
@@ -19,5 +19,8 @@ class TrackAPI extends RESTDataSource {
       getTrackModules(trackId) {
         return this.get(`track/${trackId}/modules`);
       }
+      incrementTrackViews(trackId) {
+        return this.patch(`track/${trackId}/numberOfViews`);
+     }
   }
   module.exports = TrackAPI;
